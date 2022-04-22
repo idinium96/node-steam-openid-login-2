70c0d7e4069c8b13649b93bc2aac8c8b83ef6c8d
idinium96 <47635037+idinium96@users.noreply.github.com>
2022-04-22 20:16:04 +0800

    🎨 prettify

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ module.exports = function (url, cookies, callback) {
         jar = request.jar();
 
         cookies.forEach(function (cookieStr) {
-            jar.setCookie(request.cookie(cookieStr), 'https://steamcommunity.com');
+            jar.setCookie(
+                request.cookie(cookieStr),
+                'https://steamcommunity.com'
+            );
         });
     } else {
         jar = cookies;
@@ -24,61 +27,71 @@ module.exports = function (url, cookies, callback) {
 
     // Go to path for signing in through Steam and follow redirects
 
-    request({
-        method: 'GET',
-        url: url,
-        jar: jar,
-        followAllRedirects: true
-    }, function (err, response, body) {
-        if (err) {
-            return callback(err);
-        }
-
-        if (response.request.uri.host !== 'steamcommunity.com') {
-            return callback(new Error('Was not redirected to steam, make sure the url is correct'));
-        }
-
-        const $ = cheerio.load(body);
-
-        // If we are given a login form, then we are not signed in to steam
-        if ($('#loginForm').length !== 0) {
-            return callback(new Error('You are not signed in to Steam'));
-        }
-
-        const form = $('#openidForm');
-
-        if (form.length !== 1) {
-            return callback(new Error('Could not find OpenID login form'));
-        }
-
-        const inputs = form.find('input');
-
-        const formData = {};
-
-        // Get form data
-        inputs.each(function (index, element) {
-            const attribs = element.attribs;
-            if (attribs.name) {
-                formData[attribs.name] = attribs.value;
-            }
-        });
-
-        // Send form to steam and follow redirects back to the website we are signing in to
-
-        request({
-            method: 'POST',
-            url: 'https://steamcommunity.com/openid/login',
-            form: formData,
+    request(
+        {
+            method: 'GET',
+            url: url,
             jar: jar,
-            followAllRedirects: true
-        }, function (err, response, body) {
+            followAllRedirects: true,
+        },
+        function (err, response, body) {
             if (err) {
                 return callback(err);
             }
 
-            // Return cookie jar
+            if (response.request.uri.host !== 'steamcommunity.com') {
+                return callback(
+                    new Error(
+                        'Was not redirected to steam, make sure the url is correct'
+                    )
+                );
+            }
 
-            callback(null, jar);
-        });
-    });
+            const $ = cheerio.load(body);
+
+            // If we are given a login form, then we are not signed in to steam
+            if ($('#loginForm').length !== 0) {
+                return callback(new Error('You are not signed in to Steam'));
+            }
+
+            const form = $('#openidForm');
+
+            if (form.length !== 1) {
+                return callback(new Error('Could not find OpenID login form'));
+            }
+
+            const inputs = form.find('input');
+
+            const formData = {};
+
+            // Get form data
+            inputs.each(function (index, element) {
+                const attribs = element.attribs;
+                if (attribs.name) {
+                    formData[attribs.name] = attribs.value;
+                }
+            });
+
+            // Send form to steam and follow redirects back to the website we are signing in to
+
+            request(
+                {
+                    method: 'POST',
+                    url: 'https://steamcommunity.com/openid/login',
+                    form: formData,
+                    jar: jar,
+                    followAllRedirects: true,
+                },
+                function (err, response, body) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    // Return cookie jar
+
+                    callback(null, jar);
+                }
+            );
+        }
+    );
 };
